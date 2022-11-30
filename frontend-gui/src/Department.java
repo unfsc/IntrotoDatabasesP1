5b12d3c78458c084534949da1d3743ca71664c92
Crimson421 <86333682+Crimson421@users.noreply.github.com>
2022-11-29 21:50:15 -0500

    Update Department.java

--- frontend-gui/src/Department.java
+++ frontend-gui/src/Department.java
@@ -1,5 +1,52 @@
 package src;
 
 public class Department {
+    //Create private variables
+    private String departmentName;
+    private String departmentNumber;
+    private String departmentDescription;
+    private String departmentInstructor;
+    private String departmentOffering_Department;
+    private String departmentHours;
+
+    //Create getters and setters
+    public String getDepartmentName() {
+        return departmentName;
+    }
+    public String getDepartmentNumber() {
+        return departmentNumber;
+    }
+    public String getDepartmentDescription() {
+        return departmentDescription;
+    }
+    public String getDepartmentInstructor() {
+        return departmentInstructor;
+    }
+    public String getDepartmentOffering_Department() {
+        return departmentOffering_Department;
+    }
+    public String getDepartmentHours() {
+        return departmentHours;
+    }
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+    public void setDepartmentNumber(String departmentCode) {
+        this.departmentNumber = departmentNumber;
+    }
+    public void setDepartmentDescription(String departmentAddress) {
+        this.departmentDescription = departmentDescription;
+    }
+
+    public void setDepartmentInstructor(String officePhone) {
+        this.departmentInstructor = departmentInstructor;
+    }
+    public void setDepartmentOffering_Department(String College) {
+        this.departmentOffering_Department = departmentOffering_Department;
+    }
+    public void setDepartmentHours(String departmentHours) {
+        this.departmentHours = departmentHours;
+    }
+
 
 }
