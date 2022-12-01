70ed71765db642b795e404aca589629490c79b08
kconv <kyleconverse17@gmail.com>
2022-12-01 10:21:14 -0500

    implementing prepared statements

--- frontend-gui/src/Main.java
+++ frontend-gui/src/Main.java
@@ -3,6 +3,7 @@ package src;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -111,36 +112,41 @@ public class Main {
 
         switch (entityType.toLowerCase()) {
             case "student":
-                pstmt = addStudent(conn);
+                addStudent(conn);
                 break;
             case "department":
-                pstmt = addDepartment(conn);
+                addDepartment(conn);
                 break;
             case "course":
-                // addCourse(conn);
+                addCourse(conn);
                 break;
             case "instructor":
-                // addInstructor(conn);
+                addInstructor(conn);
                 break;
             case "section":
-                // addSection(conn);
+                addSection(conn);
+                break;
+            case "person":
+                addPerson(conn);
                 break;
             default:
                 System.out.println("Invalid input!");
                 break;
         }
-        return pstmt;
 
+        return pstmt;
     }
 
     public static PreparedStatement addSection(Connection conn) throws SQLException {
+        PreparedStatement pstmt = null;
+
         return pstmt;
     }
 
     public static PreparedStatement addInstructor(Connection conn) throws SQLException {
         PreparedStatement pstmt = conn.prepareStatement(
-                "INSERT INTO INSTRUCTOR (SSN,N_NUMBER, DEPARTMENT, OFFICE_NO)" +
-                        "VALUES ('21-212-2121', 'N09812098', 'Computer Science', 3)");
+                "INSERT INTO INSTRUCTOR (SSN, N_NUMBER, DEPARTMENT, OFFICE_NO)" +
+                        "VALUES (? ? ? ?)");
 
         System.out.println("Enter the instructor's SSN: ");
         String ssn = getString();
@@ -155,76 +161,190 @@ public class Main {
         pstmt.setString(3, department);
         pstmt.setInt(4, officeNumber);
 
+        pstmt.executeUpdate();
+
         return pstmt;
     }
 
     public static PreparedStatement addCourse(Connection conn) throws SQLException {
+        PreparedStatement pstmt = conn.prepareStatement(
+                "INSERT INTO COURSE (COURSE_NO, COURSE_NAME, DESCRIPTION, OFFERING_DEPT, COURSE_LEVEL, COURSE_HOURS)" +
+                        "VALUES (? ? ? ? ? ?)");
+        System.out.println("Enter the course's course number: ");
+        String courseNumber = getString();
+        System.out.println("Enter the course's name: ");
+        String courseName = getString();
+        System.out.println("Enter the course's description: ");
+        String description = getString();
+        System.out.println("Enter the course's offering department: ");
+        String offeringDepartment = getString();
+        System.out.println("Enter the course's level: ");
+        String courseLevel = getString();
+        System.out.println("Enter the course's hours: ");
+        int courseHours = getInt();
+
+        pstmt.setString(1, courseNumber);
+        pstmt.setString(2, courseName);
+        pstmt.setString(3, description);
+        pstmt.setString(4, offeringDepartment);
+        pstmt.setString(5, courseLevel);
+        pstmt.setInt(6, courseHours);
+
+        pstmt.executeUpdate();
+
         return pstmt;
     }
 
     public static PreparedStatement addDepartment(Connection conn) throws SQLException {
+        // PreparedStatement pstmt = conn.prepareStatement(
+        // "INSERT INTO DEPARTMENT (DEPT_CODE, DEPT_NAME, OFFICE_NO, OFFICE_PHONE,
+        // COLLEGE)" +
+        // "VALUES (? ? ? ? ?)");
+        PreparedStatement pstmt = conn.prepareStatement(
+                "INSERT INTO DEPARTMENT (DEPT_CODE, DEPT_NAME, OFFICE_NO, OFFICE_PHONE, COLLEGE)" +
+                        "VALUES (? ? ? ? ?)");
+
+        // System.out.println("Enter the department's code: ");
+        // String deptCode = getString();
+        // System.out.println("Enter the department's name: ");
+        // String deptName = getString();
+        // System.out.println("Enter the department's office number: ");
+        // String officeNumber = getInt();
+        // System.out.println("Enter the department's office phone number: ");
+        // String officePhone = getString();
+        // System.out.println("Enter the department's college: ");
+        // String college = getString();
+
+        String deptCode = "CS";
+        String deptName = "Computer Science";
+        String officeNumber = "1167";
+        String officePhone = "904-567-1234";
+        String college = "Engineering";
+
+        pstmt.setString(1, deptCode);
+        pstmt.setString(2, deptName);
+        pstmt.setString(3, officeNumber);
+        pstmt.setString(4, officePhone);
+        pstmt.setString(5, college);
+
+        int rowsAdded = pstmt.executeUpdate();
+        System.out.println(rowsAdded + " department added successfully");
+
+        return pstmt;
+    }
+
+    public static PreparedStatement addPerson(Connection conn) throws SQLException {
+        PreparedStatement pstmt = conn.prepareStatement(
+                "INSERT INTO PERSON (SSN, PNAME, BIRTH_DATE, SEX, N_NUMBER, STREET_NO, ADDRESS_STREET, ZIP_CODE, STATE, CITY, PHONE_NUMBER, CURRENT_ADDRESS, PERMANENT_PHONE)"
+                        +
+                        "VALUES (? ? ? ? ? ? ? ? ? ? ? ? ?)");
+
+        System.out.println("Enter the person's SSN: ");
+        String ssn = getString();
+        System.out.println("Enter the person's name: ");
+        String name = getString();
+        System.out.println("Enter the person's birth date (yyyy-[m]m-[d]d): ");
+        String sbirthDate = getString();
+        Date birthDate = Date.valueOf(sbirthDate);
+        System.out.println("Enter the person's sex: ");
+        String sex = getString();
+        System.out.println("Enter the person's N-Number: ");
+        String nNumber = getString();
+        System.out.println("Enter the person's street number: ");
+        int streetNumber = getInt();
+        System.out.println("Enter the person's street address: ");
+        String address = getString();
+        System.out.println("Enter the person's zip code: ");
+        int zipCode = getInt();
+        System.out.println("Enter the person's state: ");
+        String state = getString();
+        System.out.println("Enter the person's city: ");
+        String city = getString();
+        System.out.println("Enter the person's phone number: ");
+        String phoneNumber = getString();
+        System.out.println("Enter the person's current address: ");
+        String currentAddress = getString();
+        System.out.println("Enter the person's permanent phone number: ");
+        String permanentPhone = getString();
+
+        pstmt.setString(1, ssn);
+        pstmt.setString(2, name);
+        pstmt.setDate(3, birthDate);
+        pstmt.setString(4, sex);
+        pstmt.setString(5, nNumber);
+        pstmt.setInt(6, streetNumber);
+        pstmt.setString(7, address);
+        pstmt.setInt(8, zipCode);
+        pstmt.setString(9, state);
+        pstmt.setString(10, city);
+        pstmt.setString(11, phoneNumber);
+        pstmt.setString(12, currentAddress);
+        pstmt.setString(13, permanentPhone);
+
+        pstmt.executeUpdate();
 
         return pstmt;
     }
 
     public static PreparedStatement addStudent(Connection conn) throws SQLException {
+
+        // addPerson(conn);
+
         PreparedStatement pstmt = conn.prepareStatement(
                 "INSERT INTO STUDENT (N_NUMBER, CLASS, DEGREE_TYPE)" +
                         "VALUES (?, ?, ?)");
 
         System.out.println("Enter student n-number: ");
-        String nNumber = getString();
+        String snNumber = getString();
         System.out.println("Enter student class: ");
         String className = getString();
         System.out.println("Enter student degree type: ");
         String degreeType = getString();
 
-        pstmt.setString(1, nNumber);
+        pstmt.setString(1, snNumber);
         pstmt.setString(2, className);
         pstmt.setString(3, degreeType);
+
+        pstmt.executeUpdate();
 
         return pstmt;
 
     }
 
-    private static PreparedStatement addStudentToClass(Connection conn) throws SQLException {
+    private static void addStudentToClass(Connection conn) throws SQLException {
         PreparedStatement pstmt = conn.prepareStatement(
                 "INSERT INTO ENROLLED_IN  (N_NUMBER, COURSE_NUMBER)" +
                         "VALUES (?, ?)");
 
+        System.out.println("Enter n-number: ");
+        String nNumber = getString();
         System.out.println("Enter course number: ");
         String courseName = getString();
-        System.out.println("Enter section number: ");
+        System.out.println("Enter grade earned: ");
         String sectionNumber = getString();
         System.out.println("Enter semester: ");
         String semester = getString();
         System.out.println("Enter year: ");
         String year = getString();
-        System.out.println("Enter n-number: ");
-        String nNumber = getString();
+
+        pstmt.setString(1, nNumber);
+        pstmt.setString(2, courseName);
+        pstmt.setString(3, sectionNumber);
+        pstmt.setString(4, semester);
+        pstmt.setString(5, year);
+
+        pstmt.executeUpdate();
+    }
+
+    public static PreparedStatement getStudentGPA(Connection conn) throws SQLException {
+        PreparedStatement pstmt = conn.prepareStatement(
+                "SELECT GPA FROM STUDENT WHERE N_NUMBER = ?");
 
         return pstmt;
     }
 
-    public static void getStudentGPA(Connection conn) throws SQLException {
-        PreparedStatement pstmt = conn.prepareStatement(
-                "SELECT GPA FROM STUDENT WHERE N_NUMBER = ?");
-
-        System.out.println("Enter student n-number: ");
-        String nNumber = getString();
-
-        pstmt.setString(1, nNumber);
-
-        ResultSet rs = pstmt.executeQuery();
-
-        if (rs.next()) {
-            System.out.println("Student GPA: " + rs.getString(1));
-        } else {
-            System.out.println("Student not found!");
-        }
-    }
-
     private static PreparedStatement addGrade(Connection conn) {
+
         return null;
     }
 
@@ -238,53 +358,56 @@ public class Main {
 
             printDriverInfo(conn);
 
+            String option = "";
+
             Scanner in = new Scanner(System.in);
+            while (!option.equals("7")) {
+                System.out.print("What would you like to do?\n" +
+                        "1. Add a new entity to the database\n" +
+                        "2. Add a student to a course\n" +
+                        "3. Given a student's Nnumber generate their grade report. \n" +
+                        "4. Given an instructor's Nnumber list all the course sections they have taught.\n" +
+                        "5. Given a department name or code find the courses offered.\n" +
+                        "6. Add a grade to a given student for a given course/section.\n" +
+                        "7. Exit\n");
 
-            System.out.print("What would you like to do?\n" +
-                    "1. Add a new entity to the database\n" +
-                    "2. Add a student to a course\n" +
-                    "3. Given a student's Nnumber generate their grade report. \n" +
-                    "4. Given an instructor's Nnumber list all the course sections they have taught.\n" +
-                    "5. Given a department name or code find the courses offered.\n" +
-                    "6. Add a grade to a given student for a given course/section.\n" +
-                    "7. Exit\n");
+                option = in.nextLine();
 
-            String option = in.nextLine();
-
-            switch (option) {
-                case "1":
-                    System.out.println("What kind of entity would you like to add?");
-                    String entityType = in.nextLine();
-                    PreparedStatement pstmt = addSomeEntity(entityType, conn);
-                    pstmt.executeUpdate();
-                    break;
-                case "2":
-                    pstmt = addStudentToClass(conn);
-                    break;
-                case "3":
-                    // TODO
-                    getStudentGPA(conn);
-                    break;
-                case "4":
-                    // TODO
-                    // pstmt = listTaughtSections(conn);
-                    // pstmt.executeQuery();
-                    break;
-                case "5":
-                    // TODO
-                    // pstmt = listDeptCourses(conn);
-                    // pstmt.executeQuery();
-                    break;
-                case "6":
-                    // TODO
-                    // pstmt = addGrade(conn);
-                    // pstmt.executeUpdate();
-                    break;
-                default:
-                    System.out.println("Invalid option");
-                    break;
+                switch (option) {
+                    case "1":
+                        System.out.println("What kind of entity would you like to add?");
+                        String entityType = in.nextLine();
+                        addSomeEntity(entityType, conn);
+                        break;
+                    case "2":
+                        addStudentToClass(conn);
+                        break;
+                    case "3":
+                        getStudentGPA(conn);
+                        break;
+                    case "4":
+                        // TODO
+                        // pstmt = listTaughtSections(conn);
+                        // pstmt.executeQuery();
+                        break;
+                    case "5":
+                        // TODO
+                        // pstmt = listDeptCourses(conn);
+                        // pstmt.executeQuery();
+                        break;
+                    case "6":
+                        // TODO
+                        // pstmt = addGrade(conn);
+                        // pstmt.executeUpdate();
+                        break;
+                    case "7":
+                        System.out.println("Exiting");
+                        break;
+                    default:
+                        System.out.println("Invalid option");
+                        break;
+                }
             }
-
             in.close();
             conn.close();
         } catch (SQLException e) {
